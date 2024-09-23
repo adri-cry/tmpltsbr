8f8b4995518b08770a8bbfd9bfbd61ce28e52af3
Adri <muhammadadriyuliansya@gmail.com>
2024-09-23 18:26:39 +0700

    Upload initial folder

---
+++ forms/contact.php
@@ -0,0 +1,32 @@
+<?php
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'muhammadadriyuliansya@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->subject = 'Pesan dari Form Kontak';
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'Nama');
+  $contact->add_message( $_POST['message'], 'Pesan', 10);
+
+  echo $contact->send();
+?>
